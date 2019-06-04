a63b30c3c3aee79beff12031c17e65298fad4be1
zillionchu <2538887088@qq.com>
2019-06-04 20:52:53 +0800

    20190604

---
+++ zillionchu-feigin/src/main/java/com/github/zillionchu/feign/zillionchufeigin/fegin/FeginServiceImpl.java
@@ -0,0 +1,13 @@
+//package com.github.zillionchu.feign.zillionchufeigin.fegin;
+//
+///**
+// * @Auther: ZiLlionChu
+// * @Date: 2019/3/21 10:54
+// * @Description:
+// */
+//public class FeginServiceImpl implements FeginService {
+//    @Override
+//    public String getUser() {
+//        return null;
+//    }
+//}
